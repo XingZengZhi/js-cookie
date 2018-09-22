125cc9951e191ccc169124c43c46a704b15cafba
XingZengZhi <944047118@qq.com>
2018-09-22 16:00:59 +0800

    常用工具函数更新
    增加对表单序列化成JSON格式的函数

--- js-cookie.js
+++ js-cookie.js
@@ -89,3 +89,27 @@
         }
         return ratio;
     }
+
+    /**  
+     *    表单序列化成JSON格式
+     *   
+     * @author XingZengZhi  
+     * @date 2018/8/15 12:41  
+     * @param   
+     * @return   
+     */
+    $.fn.serializeObject = function(){    
+       var o = {};    
+       var a = this.serializeArray();    
+       $.each(a, function() {    
+           if (o[this.name]) {    
+               if (!o[this.name].push) {    
+                   o[this.name] = [o[this.name]];    
+               }    
+               o[this.name].push(this.value || '');    
+           } else {    
+               o[this.name] = this.value || '';    
+           }    
+       });    
+       return o;    
+    }; 
